166c611ec6a467d53acfff10d27d28e7fc8ae61e
KyeTownsend <113002986+KyeTownsend@users.noreply.github.com>
2022-10-30 16:41:39 +1100

    Updated gamemanager, added pellets and powerpellets to tilemap

---
+++ PacStudent/Assets/Scripts/Pellet.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pellet : MonoBehaviour
+{
+
+    protected virtual void Eat() {
+        FindObjectOfType<GameManager>().PelletEaten(this);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("eaten");
+        Eat();
+    }
+
+}
